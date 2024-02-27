c9617ea320d671ab5e5ce961877392c658a29411
彩姫 三澤 <220285@jc-21.jp>
2024-02-27 16:35:22 +0900

    文微調整

--- ClientUDP.cpp
+++ ClientUDP.cpp
@@ -11,37 +11,41 @@ const unsigned short SERVERPORT = 8888;
 // 送受信するメッセージの最大値
 const unsigned int MESSAGELENGTH = 1024;
 
+using std::cin;
+using std::cout;
+using std::endl;
+
 // WinSockの初期化
 int InitWinSock();
 
 int main()
 {
-	std::cout << "Chat Client" << std::endl;
+	cout << "Chat Client" << endl;
 
 	WSADATA wsaData;
 	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (ret != 0)
 	{
-		std::cout << "Error: InitWinSock ( ErrorCode:" << ret << " )" << std::endl;
-		return 1;	// おーわり！
+		cout << "Error: InitWinSock ( ErrorCode:" << ret << " )" << endl;
+		return 1;	// 終了
 	}
-	std::cout << "Success: InitWinSock" << std::endl;
+	cout << "Success: InitWinSock" << endl;
 
 
 	// UDPソケットの作成
 	int sock;
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
-		std::cout << "Error: socket ( ErrorCode:" << WSAGetLastError() << " )" << std::endl;
+		cout << "Error: socket ( ErrorCode:" << WSAGetLastError() << " )" << endl;
 		return 1;	// おーわり！
 	}
-	std::cout << "Success: socket" << std::endl;
+	cout << "Success: socket" << endl;
 
 
 	// 宛先となるサーバのソケットアドレス情報の割り当て
 	std::string serverIpv4Address;
-	std::cout << "Input Server IPv4 Address : ";
-	std::cin >> serverIpv4Address;
+	cout << "Input Server IPv4 Address : ";
+	cin >> serverIpv4Address;
 	struct sockaddr_in toAddr;
 	memset(&toAddr, 0, sizeof(toAddr));
 	toAddr.sin_family = AF_INET;
@@ -57,29 +61,29 @@ int main()
 
 
 		// 送信用メッセージの入力
-		std::cout << "Input message : ";
-		std::cin >> buff;
+		cout << "Input message : ";
+		cin >> buff;
 
 		// 送信！
 		ret = sendto(sock, buff, strlen(buff), 0, (struct sockaddr*)&toAddr, sizeof(toAddr));
 		if (ret != strlen(buff))
 		{
-			std::cout << "Error: sendto ( ErrorCode:" << WSAGetLastError() << " )" << std::endl;
+			cout << "Error: sendto ( ErrorCode:" << WSAGetLastError() << " )" << endl;
 			return 1;
 		}
 
 		// 受信待ち
-		std::cout << "wait..." << std::endl;
+		cout << "wait..." << endl;
 
 		// 受信	\0は送ってこないバージョン
 		ret = recvfrom(sock, buff, sizeof(buff) - 1, 0, (struct sockaddr*)&fromAddr, &fromlen);
 		if (ret < 0)
 		{
-			std::cout << "Error: recvfrom ( ErrorCode:" << WSAGetLastError() << " )" << std::endl;
+			cout << "Error: recvfrom ( ErrorCode:" << WSAGetLastError() << " )" << endl;
 			return 1;
 		}
 		buff[ret] = '\0';	// 終端記号追加
-		std::cout << "Receive message : " << buff << std::endl;
+		cout << "Receive message : " << buff << endl;
 	}
 
 	return 0;
